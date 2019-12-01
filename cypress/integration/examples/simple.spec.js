5977d57ce43591aaadda290710d23473bc869077
Alexander Rose <arose728@gmail.com>
2019-12-01 17:47:07 -0500

    initial

--- cypress/integration/examples/simple.spec.js
+++ cypress/integration/examples/simple.spec.js
@@ -1,28 +1,18 @@
 /// <reference types="Cypress" />
 
-context('simple test to order chipotle', () => {
-  beforeEach(() => {
-    cy.visit('https://order.chipotle.com')
+context('simple test to google sunflowers', () => {
+  before(() => {
+    cy.visit('www.google.com')
   });
-
-  it('click on location', () => {
-    cy.get('.ng-pristine .ng-valid .ng-empty ng-touched')
-      .type('07302').should('have.value', '07302')
-    //
-    //   // Delay each keypress by 0.1 sec
-    //   .type('slow.typing@email.com', { delay: 100 })
-    //   .should('have.value', 'slow.typing@email.com');
-    //
-    // cy.get('.action-disabled')
-    //   // Ignore error checking prior to type
-    //   // like whether the input is visible or disabled
-    //   .type('disabled error checking', { force: true })
-    //   .should('have.value', 'disabled error checking')
+    it('enter sunflower', () => {
+      cy.get('.gLFyf')
+          .type('sunflower').should('have.value', 'sunflower')
+    });
+    it('click on search', () => {
+      cy.contains('Google Search').click();
+    // cy.get('.gNO89b').click();
+    });
+    it('searching images', () => {
+      cy.contains('Images').click();
   });
-  //
-  // it('.focus() - focus on a DOM element', () => {
-  //   cy.get('.action-focus').focus()
-  //     .should('have.class', 'focus')
-  //     .prev().should('have.attr', 'style', 'color: orange;')
-  // });
 });
