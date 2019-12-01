9b5adfac039d831978f04c62ef29ed5772228fb5
Alexander Rose <arose728@gmail.com>
2019-12-01 18:19:59 -0500

    initial

--- cypress/integration/examples/simple.spec.js
+++ cypress/integration/examples/simple.spec.js
@@ -4,15 +4,15 @@ context('simple test to google sunflowers', () => {
   before(() => {
     cy.visit('www.google.com')
   });
-    it('enter sunflower', () => {
-      cy.get('.gLFyf')
-          .type('sunflower').should('have.value', 'sunflower')
-    });
-    it('click on search', () => {
-      cy.contains('Google Search').click();
+  it('enter sunflower', () => {
+    cy.get('.gLFyf')
+        .type('sunflower').should('have.value', 'sunflowe')
+  });
+  it('click on search', () => {
+    cy.contains('Google Search').click();
     // cy.get('.gNO89b').click();
-    });
-    it('searching images', () => {
-      cy.contains('Images').click();
+  });
+  it('searching images', () => {
+    cy.contains('Images').click();
   });
 });
