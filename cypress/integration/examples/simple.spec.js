8e1fd7bc3e0909ab0fa611df8aa6ea1d47b16f70
Alexander Rose <arose728@gmail.com>
2019-12-01 16:26:19 -0500

    initial

--- cypress/integration/examples/simple.spec.js
+++ cypress/integration/examples/simple.spec.js
@@ -8,16 +8,16 @@ context('simple test to order chipotle', () => {
   it('click on location', () => {
     cy.get('.ng-pristine .ng-valid .ng-empty ng-touched')
       .type('07302').should('have.value', '07302')
-
-      // Delay each keypress by 0.1 sec
-      .type('slow.typing@email.com', { delay: 100 })
-      .should('have.value', 'slow.typing@email.com');
-
-    cy.get('.action-disabled')
-      // Ignore error checking prior to type
-      // like whether the input is visible or disabled
-      .type('disabled error checking', { force: true })
-      .should('have.value', 'disabled error checking')
+    //
+    //   // Delay each keypress by 0.1 sec
+    //   .type('slow.typing@email.com', { delay: 100 })
+    //   .should('have.value', 'slow.typing@email.com');
+    //
+    // cy.get('.action-disabled')
+    //   // Ignore error checking prior to type
+    //   // like whether the input is visible or disabled
+    //   .type('disabled error checking', { force: true })
+    //   .should('have.value', 'disabled error checking')
   });
   //
   // it('.focus() - focus on a DOM element', () => {
